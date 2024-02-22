2ef4ee90a6d615b200d6bd7dfadfda06c7a403f8
trangqtt <quach.trang@outlook.com>
2024-02-22 21:54:56 +0700

    fix complete

--- vendor/js/main.js
+++ vendor/js/main.js
@@ -1,8 +1,9 @@
-var navbar_toggler = document.querySelector('.navbar-toggler'),
-    menu = document.querySelector('.navbar-mobi'),
+const navbar_toggler = document.querySelector('.icon-bar'),
+    menu = document.querySelector('.navbar-mobile'),
     menu_scroll = document.querySelector('.menu');
 navbar_toggler.addEventListener('click', function() {
-    menu.classList.toggle('show');
+    navbar_toggler.classList.toggle('is-active');
+    menu.classList.toggle('is-show');
 })
 window.addEventListener('scroll', function() {
     if (window.pageYOffset > 10) {
